09fa5b09bcef0f93afdad6a6121e91a4051c77d4
martingranak <granak3b@gmail.com>
2019-04-28 10:13:39 +0200

    First commit

---
+++ Models/TodoContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    using MongoDB.Driver;
+    using TodoApp.Config;
+
+    public class TodoContext : ITodoContext
+    {
+        private readonly IMongoDatabase _db;
+
+        public TodoContext(MongoDBConfig config)
+        {
+            var client = new MongoClient(config.ConnectionString);
+            _db = client.GetDatabase(config.Database);
+        }
+
+        public IMongoCollection<Todo> Todos => _db.GetCollection<Todo>("Todos");
+    }
+}
---
+++ Repository/ITodoRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Repository
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using TodoApp.Models;
+
+    public interface ITodoRepository
+    {
+        // api/[GET]
+        Task<IEnumerable<Todo>> GetAllTodos();
+
+        // api/1/[GET]
+        Task<Todo> GetTodo(long id);
+
+        // api/[POST]
+        Task Create(Todo todo);
+
+        // api/[PUT]
+        Task<bool> Update(Todo todo);
+
+        // api/1/[DELETE]
+        Task<bool> Delete(long id);
+
+        Task<long> GetNextId();
+    }
+}
